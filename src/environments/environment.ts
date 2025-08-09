5791c2e1e846ec65679679b1f3bb05e6c6a2cb2e
miguelrc <mrcabellos@hotmail.com>
2025-08-09 10:25:03 -0500

    config huascaran

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     //webAPI: 'https://argozenware.com/dev/backend/api/',
-    webAPI: 'https://sigzenware.com/bac/api/',
+    webAPI: 'https://ion.net.pe/huascaran-backend/api/',
     msalConfig: {
         auth: {
             clientId: 'b65e275c-ca73-4aac-b3e3-fd74c0658fd8',
