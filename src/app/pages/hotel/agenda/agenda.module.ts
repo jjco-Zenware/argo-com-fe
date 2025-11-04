0b8457611f2188ac65fb8fadb22fea4571ade47d
juan jose <jjcastilloo79@gmail.com>
2025-11-04 14:16:42 -0500

    jjco agenda

---
+++ src/app/pages/hotel/agenda/agenda.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SharedPrimeNgModule } from '@primeNgModule';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { KnobModule } from 'primeng/knob';
+import { FormsModule } from '@angular/forms';
+import { MsalModule } from '@azure/msal-angular';
+import { SharedAppModule } from 'src/app/shared/shared-App.module';
+import { SharedAppService } from '@sharedAppService';
+import { TimelineModule } from 'primeng/timeline';
+import { FileUploadModule } from 'primeng/fileupload';
+import { CAgendaComponent } from './c-agenda/c-agenda.component';
+import { AgendaRoutingModule } from './agenda-routing.module';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { CModalAgendaComponent } from './modalagenda/c-modalagenda.component';
+import { DialogService } from 'primeng/dynamicdialog';
+
+
+@NgModule({
+  declarations: [
+    CAgendaComponent,
+    CModalAgendaComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedPrimeNgModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    KnobModule,
+    FormsModule,
+    MsalModule,
+    SharedAppModule,
+    TimelineModule,
+    FileUploadModule,
+    AgendaRoutingModule,
+    FullCalendarModule
+  ],
+  providers: [
+    
+    SharedAppService,
+    //ListatareasService,
+    DialogService
+  ]
+})
+export class AgendaModule { }
